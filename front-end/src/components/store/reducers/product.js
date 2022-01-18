293da0f6910f0b5e974fd2fe07aa411b4363e60a
Bruno Franco <brunofvilela1996@gmail.com>
2022-01-18 15:16:32 -0300

    Initial commit

---
+++ front-end/src/components/store/reducers/product.js
@@ -0,0 +1,136 @@
+const productsList = [
+  {
+    id_product: 1,
+    category: 'olympikus',
+    fk_idcategorys: 1,
+    name_product: 'Tênis Olympikus Flash - Masculino',
+    price: 300,
+    image: './images/produtos/olympikus-01.jpg',
+    id_categorys: 1,
+    name_categorys: 'Tenis Olympikus',
+    selected: false
+  },
+  {
+    id_product: 2,
+    category: 'olympikus',
+    fk_idcategorys: 1,
+    name_product: 'Tênis Olympikus Flash - Masculino',
+    price: 350,
+    image: './images/produtos/olympikus-02',
+    id_categorys: 1,
+    name_categorys: 'Tenis Olympikus',
+    selected: false
+  },
+  {
+    id_product: 9,
+    category: 'olympikus',
+    fk_idcategorys: 1,
+    name_product: 'Tênis Olympikus Flash - Masculino',
+    price: 230,
+    image: './images/produtos/olympikus-03',
+    id_categorys: 1,
+    name_categorys: 'Tenis Olympikus',
+    selected: false
+  },
+  {
+    id_product: 4,
+    category: 'olympikus',
+    fk_idcategorys: 2,
+    name_product: 'Tênis Olympikus Flash - Masculino',
+    price: 399.99,
+    image: './images/produtos/olympikus-04',
+    id_categorys: 1,
+    name_categorys: 'Tenis Olympikus',
+    selected: false
+  },
+  {
+    id_product: 10,
+    category: 'Mizuno',
+    fk_idcategorys: 2,
+    name_product: 'Tênis Mizuno Wave Invictus - Masculino',
+    price: 350.99,
+    image: './images/produtos/mizuno-01.webp',
+    id_categorys: 2,
+    name_categorys: 'Tenis Mizuno',
+    selected: false
+  },
+  {
+    id_product: 11,
+    category: 'Mizuno',
+    fk_idcategorys: 2,
+    name_product: 'Tênis Mizuno Wave Prophecy X - Masculino',
+    price: 299.99,
+    image: './images/produtos/mizuno-02.webp',
+    id_categorys: 2,
+    name_categorys: 'Tenis Mizuno',
+    selected: false
+  },
+  {
+    id_product: 6,
+    category: 'Mizuno',
+    fk_idcategorys: 3,
+    name_product: 'Tênis Mizuno Wave Prophecy X - Masculino',
+    price: 200,
+    image: './images/produtos/mizuno-03.webp',
+    id_categorys: 2,
+    name_categorys: 'Tenis Mizuno',
+    selected: false
+  },
+  {
+    id_product: 7,
+    category: 'Mizuno',
+    fk_idcategorys: 3,
+    name_product: 'Tênis Asics Gel-Nagoya 3 Marvel - Masculino',
+    price: 450,
+    image: './images/produtos/mizuno-04.webp',
+    id_categorys: 2,
+    name_categorys: 'Tenis Mizuno',
+    selected: false
+  },
+  {
+    id_product: 3,
+    category: 'Nike',
+    fk_idcategorys: 4,
+    name_product: 'Tênis Nike Downshifter 11 - Masculino',
+    price: 350,
+    image: './images/produtos/nike-01.webp',
+    id_categorys: 3,
+    name_categorys: 'Tenis Nike',
+    selected: false
+  },
+  {
+    id_product: 5,
+    category: 'Nike',
+    fk_idcategorys: 4,
+    name_product: 'Tênis Nike Air Zoom Pegasus 38 - Masculino',
+    price: 399,
+    image: './images/produtos/nike-02.webp',
+    id_categorys: 3,
+    name_categorys: 'Tenis Nike',
+    selected: false
+  },
+  {
+    id_product: 8,
+    category: 'Nike',
+    fk_idcategorys: 5,
+    name_product: 'Tênis Nike Legend Essential 2 - Masculino',
+    price: 180,
+    image: './images/produtos/nike-03.webp',
+    id_categorys: 3,
+    name_categorys: 'Tenis Nike',
+    selected: false
+  },
+  {
+    id_product: 12,
+    category: 'Nike',
+    fk_idcategorys: 5,
+    name_product: 'Tênis Nike Revolution 5 - Masculino',
+    price: 189.99,
+    image: './images/produtos/nike-04.webp',
+    id_categorys: 3,
+    name_categorys: 'Tenis Nike',
+    selected: false
+  }
+]
+
+export default productsList
